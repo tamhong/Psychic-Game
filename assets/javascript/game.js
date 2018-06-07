b54d17813df4418677eae65dc95c6fe0291eb60a
Tam Hong <tamhong.sd@gmail.com>
2018-06-06 17:47:34 -0700

    finished game with no styling

---
+++ assets/javascript/game.js
@@ -0,0 +1,51 @@
+var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+var computerChoice = letters[Math.floor(Math.random() * letters.length)];
+
+var wins = 0;
+var losses = 0;
+var guesses = 9;
+var lettersGuessed= [];
+
+
+window.onload = function(start) {
+
+    document.onkeypress = function(event) {
+        var userGuess = event.key;
+        lettersGuessed.push(userGuess);
+
+        if(userGuess === computerChoice){
+            wins++;
+            guesses=9;
+        } else{
+            guesses--;
+        }
+        if(guesses === 0){
+            losses++;
+            guesses=9;
+        }
+        if(guesses===9){
+            computerChoice = letters[Math.floor(Math.random() * letters.length)];
+            lettersGuessed = [];
+        }
+
+        document.getElementById('wins').innerHTML = "Wins: " +wins;
+        document.getElementById('losses').innerHTML = "Losses: " +losses;
+        document.getElementById('guesses').innerHTML = "Guesses Left: " +guesses;
+        document.getElementById('lettersGuessed').innerHTML = "Letters Guessed: " +lettersGuessed.join(', ');
+
+    }
+
+    document.getElementById('wins').innerHTML = "Wins: " +wins;
+    document.getElementById('losses').innerHTML = "Losses: " +losses;
+    document.getElementById('guesses').innerHTML = "Guesses Left: " +guesses;
+    document.getElementById('lettersGuessed').innerHTML = "Letters Guessed: ";
+
+}
+
+
+
+
+
+
+
