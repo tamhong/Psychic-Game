df5e21a0ded1f35a17d0653b48097632d0abe664
Tam Hong <tamhong.sd@gmail.com>
2018-06-07 19:19:41 -0700

    final draft with comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,18 @@
+//Array of letters to be randomly generated for computerChoice
 var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
+//Initial generated letter 
 var computerChoice = letters[Math.floor(Math.random() * letters.length)];
 
+//Main variable declaration
 var wins = 0;
 var losses = 0;
 var guesses = 9;
 var lettersGuessed= [];
 
-
+//So that the text will display at initial loading of page
 window.onload = function(start) {
-
+    //Main game function
     document.onkeypress = function(event) {
         var userGuess = event.key;
         lettersGuessed.push(userGuess);
@@ -28,6 +31,8 @@ window.onload = function(start) {
             computerChoice = letters[Math.floor(Math.random() * letters.length)];
             lettersGuessed = [];
         }
+
+        //Updated game results
 
         document.getElementById('wins').innerHTML = "Wins: " +wins;
         document.getElementById('losses').innerHTML = "Losses: " +losses;
